0e24dfb2f453b120607760f8c19551c1ecbde8c9
MudassarKhop <khopatkarmudassar@gmail.com>
2022-07-27 16:28:26 +0200

    3rd push

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -49,4 +49,54 @@ router.get("/:id", (req, res) => {
 		res.status(400).send(error);
 	}
 });
+router.put("/:id", (req, res) => {
+	const {
+		email,
+		password,
+		full_name,
+		billing_address,
+		default_shipping_address,
+		country,
+		phone,
+		user_type,
+	} = req.body;
+	try {
+		con.query(
+			`UPDATE users SET email="${email}", password="${password}", full_name="${full_name}", billing_address="${billing_address}", default_shipping_address="${default_shipping_address}", country="${country}", phone="${phone}", user_type="${user_type}" WHERE user_id= ${req.params.id}`,
+			(err, result) => {
+				if (err) throw err;
+				res.send(result);
+			}
+		);
+	} catch (error) {
+		console.log(error);
+	}
+});
+router.get("/:id", (req, res) => {
+	try {
+		con.query(
+			`SELECT * FROM users where user_id= ${req.params.id} `,
+			(err, result) => {
+				if (err) throw err;
+				res.send(result);
+			}
+		);
+	} catch (error) {
+		console.log(error);
+		res.status(400).send(error);
+	}
+});
+router.delete("/:id", (req, res) => {
+	try {
+		con.query(
+			`Delete from users WHERE user_id= ${req.params.id}`,
+			(err, result) => {
+				if (err) throw err;
+				res.send(result);
+			}
+		);
+	} catch (error) {
+		console.log(error);
+	}
+});
 module.exports = router;
